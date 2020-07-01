44dc50503cbb1cc238c850cc19d409641c58727a
Jeffrey Kraemer <jeffreykraemer@factorization.net>
2020-07-01 16:42:28 -0700

    renamed iterm to iterm2

---
+++ iterm2/setup.sh
@@ -0,0 +1,25 @@
+#! /usr/bin/env sh
+
+DIR=$(dirname "$0")
+cd "$DIR"
+
+. ../scripts/functions.sh
+
+SOURCE="$(realpath -m .)"
+DESTINATION="$(realpath -m ~/.config/iterm2)"
+
+info "Configuring iterm2..."
+
+substep_info "Creating iterm2 config directory..."
+mkdir -p "$DESTINATION"
+
+find . -name "*.plist" | while read fn; do
+    fn=$(basename $fn)
+    symlink "$SOURCE/$fn" "$DESTINATION/$fn"
+done
+
+substep_info "Setting preferences custom folder..."
+defaults write ~/Library/Preferences/com.googlecode.iterm2 LoadPrefsFromCustomFolder -bool true
+defaults write ~/Library/Preferences/com.googlecode.iterm2 PrefsCustomFolder -string "$DESTINATION"
+
+success "Finished configuring iterm2."
