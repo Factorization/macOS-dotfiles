4e52bcc6a1061e1fa800b31c458be03f3753b328
Jeffrey Kraemer <jeffreykraemer@factorization.net>
2020-07-01 21:15:17 -0700

    Test argv

--- fish/my_functions.fish
+++ fish/my_functions.fish
@@ -54,6 +54,7 @@ complete -c cheat.sh -xa '(curl -s cheat.sh/:list)'
 
 function venv
     if ! test -d ~/.virtualenvs/(basename $PWD)
+        echo $argv
         vf new $argv (basename $PWD)
         vf connect
         python3 -m pip install --upgrade pip setuptools
