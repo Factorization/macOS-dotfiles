b2d2ae7d24240a9e9dc42a17cfa8b0a42cc6f2fd
Jeffrey Kraemer <jeffreykraemer@factorization.net>
2020-07-02 13:36:13 -0700

    Updated Vim config

--- vim/setup.sh
+++ vim/setup.sh
@@ -5,14 +5,20 @@ cd "$DIR"
 
 . ../scripts/functions.sh
 
+SOURCE_REPO = "https://github.com/amix/vimrc.git"
 SOURCE="$(realpath -m .)"
-DESTINATION="$(realpath -m ~)"
+DESTINATION="$(realpath -m ~/.vim_runtime)"
 
 info "Setting up Vim..."
 
-find . -name ".vim*" | while read fn; do
-    fn=$(basename $fn)
-    symlink "$SOURCE/$fn" "$DESTINATION/$fn"
-done
+# find . -name ".vim*" | while read fn; do
+#     fn=$(basename $fn)
+#     symlink "$SOURCE/$fn" "$DESTINATION/$fn"
+# done
+substep_info "Cloning ultimate vimrc..."
+git clone --depth=1 "$SOURCE_REPO" "$DESTINATION" 2>/dev/null || git -C "$DESTINATION" pull --rebase
+
+substep_info "Running setup..."
+sh "$DESTINATION/install_awesome_vimrc.sh"
 
 success "Finished setting up Vim."
