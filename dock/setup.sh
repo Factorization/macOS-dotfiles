22ccf2f97e21587d3dd8575dbf3b9b2e48f6686a
Jeffrey Kraemer <jeffreykraemer@factorization.net>
2020-07-03 09:44:32 -0700

    Added more text and killall Dock.

--- dock/setup.sh
+++ dock/setup.sh
@@ -11,8 +11,12 @@ DESTINATION="$(realpath -m ~/Library/Preferences)"
 info "Copying dock configuration..."
 
 find * -name "*.plist" | while read fn; do
+    substep_info "Copying dock preference list..."
     cp -f "$SOURCE/$fn" "$DESTINATION/$fn"
+    substep_info "Reading dock config..."
     defaults read "$DESTINATION/$fn" &>/dev/null
+    substep_info "Restarting dock..."
+    killall Dock
 done
 
 success "Finished copying dock configuration."
