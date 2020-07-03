d7189d4028d99921013399ba97f8d0a403dbe689
Jeffrey Kraemer <jeffreykraemer@factorization.net>
2020-07-03 09:43:27 -0700

    Added termal config.

---
+++ terminal/setup.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/env sh
+
+DIR=$(dirname "$0")
+cd "$DIR"
+
+. ../scripts/functions.sh
+
+SOURCE="$(realpath -m .)"
+DESTINATION="$(realpath -m ~/Library/Preferences)"
+
+info "Copying terminal configuration..."
+
+find * -name "*.plist" | while read fn; do
+    substep_info "Copying terminal preference list..."
+    symlink "$SOURCE/$fn" "$DESTINATION/$fn"
+    substep_info "Note: Terminal will need to be restarted to take effect."
+done
+
+success "Finished copying Terminal configuration."
