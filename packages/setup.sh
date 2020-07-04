81b809562f82d5f5229564a5fa8413eecdfd6ca5
Jeffrey Kraemer <jeffreykraemer@factorization.net>
2020-07-04 00:03:56 -0700

    Removed unneeded code.

--- packages/setup.sh
+++ packages/setup.sh
@@ -14,27 +14,3 @@ fi
 info "Installing Brewfile packages..."
 brew bundle
 success "Finished installing Brewfile packages."
-
-find * -name "*.list" -not -wholename "*global*" | while read fn; do
-    cmd="${fn%.*}"
-    set -- $cmd
-    info "Installing $1 packages..."
-    while read package; do
-        if [[ $package == $COMMENT ]]; then continue; fi
-        substep_info "Installing $package..."
-        $cmd $package
-    done < "$fn"
-    success "Finished installing $1 packages."
-done
-
-find * -name "*global.list" | while read fn; do
-    cmd="${fn%.*}"
-    set -- $cmd
-    info "Setting $1 global version..."
-    while read version; do
-        if [[ $version == $COMMENT ]]; then continue; fi
-        substep_info "to $version..."
-        $cmd $version
-    done < "$fn"
-    success "Finished setting $1 global version."
-done
