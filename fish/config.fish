59ddc82044e8cec95df588d4917cc6c671512838
Jeffrey Kraemer <jeffreykraemer@factorization.net>
2021-07-02 12:47:00 -0700

    UPdated fish function

--- fish/config.fish
+++ fish/config.fish
@@ -4,7 +4,7 @@ source ~/.config/fish/my_functions.fish
 # Install fisher (the fish package manager)
 if not functions -q fisher
     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    curl -sL https://git.io/fisher | source 
     fish -c fisher
 end
 
